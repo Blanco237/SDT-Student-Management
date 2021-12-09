8f59748166ef3e07e04406edfaac8726db24bbc9
Blanco237 <asongrandy9@gmail.com>
2021-12-09 11:36:31 +0100

    Some pages done

---
+++ ProjectFiles/src/main/java/com/projectfiles/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.projectfiles;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        new WelcomePage().setVisible(true);
+        System.out.println("Stuff");
+        WelcomePage home = new WelcomePage();
+        home.setVisible(true);
+    }
+    
+}
